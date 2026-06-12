0323d4126e66d083ced817c287f8b8526f9bbf0d
Aigiz Tulubaev <flaylay256@gmail.com>
2026-06-12 20:37:00 +0300

    hotfix: update exception

---
+++ java-backend/src/main/java/com/sprintly/backend/exception/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package com.sprintly.backend.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
